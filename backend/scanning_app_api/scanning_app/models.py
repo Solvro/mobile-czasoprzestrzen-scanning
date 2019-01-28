83731ed8abbc9168df095d2fc52708cec0296b3b
Starzeco <marcel.k@onet.eu>
2019-01-28 19:13:21 +0100

    feat(models): models added

--- backend/scanning_app_api/scanning_app/models.py
+++ backend/scanning_app_api/scanning_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+TYPE = (
+    ("Mic", "Microphone"),
+    ("Gui", "Guitar"),
+    ("Spe", "Speakers"),
+
+)
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    phone = PhoneNumberField()
+    address = models.CharField(null=True, max_length=255)
+    business_data = models.CharField(null=True, max_length=255)
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=400)
+    status = models.BooleanField()
+    type = models.CharField(choices=TYPE, max_length=64)
+    max_rent_time = models.DurationField()
+
+
+class RentalInfo(models.Model):
+    rental_date = models.DateField(auto_now=True)
+    expected_return = models.DateField()
+    actual_return = models.DateField(null=True)
+    equipment_data_id = models.ForeignKey(Equipment, on_delete=models.DO_NOTHING)
+    client_data_id = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
--- backend/scanning_app_api/scanning_app/tests.py
+++ backend/scanning_app_api/scanning_app/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Client
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        self.client_first_name = "Marcel"
+        self.client_last_name = "Krakowiak"
+        self.client_email = "marcel.k@onet.eu"
+        self.client_phone = "793381442"
+        self.client_address = "Nowaczka 1/43"
+        self.client = Client(first_name=self.client_first_name, last_name=self.client_last_name, email=self.client_email, phone=self.client_phone, address=self.client_address)
+
+    def test_model_create_client(self):
+        old_count = Client.objects.count()
+        self.client.save()
+        new_count = Client.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
