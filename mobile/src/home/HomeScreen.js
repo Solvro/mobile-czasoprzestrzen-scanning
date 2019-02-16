63d60a6ec04a29f579c7848bb542df06fa08c589
Wiktoria <wiktoriajuliasalamon@gmail.com>
2019-02-16 21:47:36 +0100

    build(home_screen): home screen with references to rent, return, equipment

--- mobile/src/home/HomeScreen.js
+++ mobile/src/home/HomeScreen.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Container, Text} from 'native-base';
+import {View, Animated, TouchableOpacity, TouchableWithoutFeedback} from 'react-native';
+
+import homeStyles from '../styles/HomeStyles.js';
+import logo from '../assets/logo.jpg';
 
 export default class HomeScreen extends React.Component {
 
     constructor(props) {
         super(props);
+        this.imageHeight = new Animated.Value(200);
     }
 
     render() {
         return(
-            <View>
-                <Text>It's another screen of app!</Text>
-            </View>
+            <Container>
+                <TouchableWithoutFeedback>
+                    <Animated.View style={homeStyles.background}>
+                        <View style={homeStyles.logoContainer}>
+                            <Animated.Image source={logo} resizeMode='contain'
+                                style={[homeStyles.logoStyle,
+                                      {height: this.imageHeight}]} />
+                            <Animated.Text style={homeStyles.title}>
+                              Czasoprzestrzeń
+                            </Animated.Text>
+                      </View>
+                        <TouchableOpacity style={homeStyles.actionButton} onPress={() => this.props.navigation.navigate('Rent')}>
+                            <Text style={homeStyles.buttonText}> Wypożycz</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={homeStyles.actionButton} onPress={() => this.props.navigation.navigate('Return')}>
+                            <Text style={homeStyles.buttonText}> Zwróć</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={homeStyles.actionButton} onPress={() => this.props.navigation.navigate('Equipment')}>
+                            <Text style={homeStyles.buttonText}> Lista sprzętu</Text>
+                        </TouchableOpacity>
+                  </Animated.View>
+              </TouchableWithoutFeedback>
+          </Container>
         )
     }
 }
---
+++ mobile/src/styles/HomeStyles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+const homeStyles = StyleSheet.create({
+
+    title: {
+        paddingTop: 10,
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+
+   background: {
+    flex: 1,
+    },
+
+    actionButton: {
+        alignItems: 'center',
+        marginTop: 25,
+        backgroundColor: '#3b82c4',
+        marginRight: 50,
+        marginLeft: 50,
+        padding: 10,
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontSize: 27,
+    },
+
+   logoContainer: {
+        alignItems: 'center',
+        marginTop: 30,
+        marginBottom: 30,
+    },
+
+    logoStyle: {
+        width: 190,
+        height: 80,
+    }
+});
+
+module.exports = homeStyles;
