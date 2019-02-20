50893428bc99a50015b35bbae57b95b9b4d4f81a
Starzeco <marcel.k@onet.eu>
2019-02-20 19:24:13 +0100

    feat: add simple custom permission

--- backend/scanning_app/views.py
+++ backend/scanning_app/views.py
@@ -1,7 +1,8 @@
+from .permissions import PostPermissions
 from .serializers import EquipmentSerializer, ClientSerializer, RentalInfoSerializer
 from .models import Equipment, Client, RentalInfo
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -9,13 +10,16 @@ from rest_framework import viewsets
 class EquipmentView(viewsets.ModelViewSet):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class ClientView(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = (PostPermissions,)
 
 
 class RentalInfoView(viewsets.ModelViewSet):
     queryset = RentalInfo.objects.all()
     serializer_class = RentalInfoSerializer
+    permission_classes = (IsAuthenticated,)
