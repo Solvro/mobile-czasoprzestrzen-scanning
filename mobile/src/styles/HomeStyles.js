10388ffec05ce63f3a141437b1b999f92f7ef335
Wiktoria Salamon <wiktoriajuliasalamon@gmail.com>
2019-02-20 08:31:57 +0000

    Profile screen

--- mobile/src/styles/HomeStyles.js
+++ mobile/src/styles/HomeStyles.js
@@ -14,11 +14,32 @@ const homeStyles = StyleSheet.create({
 
     actionButton: {
         alignItems: 'center',
-        marginTop: 25,
+        marginTop: 5,
         backgroundColor: '#3b82c4',
         marginRight: 50,
         marginLeft: 50,
         padding: 10,
+        overflow: 'hidden',
+        borderRadius: 10,
+        flexDirection: 'row',
+        width: 250,
+    },
+
+    buttonContainer: {  
+        alignItems: 'center', 
+        shadowColor: '#0d4579',
+        shadowOffset: {width: 5, height: 5},
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        elevation: 3,
+        zIndex: 999,
+        padding: 20,
+    },
+
+    textContainer: {
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: 190,
     },
 
     buttonText: {
@@ -35,7 +56,11 @@ const homeStyles = StyleSheet.create({
     logoStyle: {
         width: 190,
         height: 80,
-    }
+    },
+    icons: {
+        fontSize: 27,
+        color: '#d9e1e8',
+    },
 });
 
 module.exports = homeStyles;
