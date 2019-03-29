587dcb567373a574339de5120432334cd716f37d
Milosz Korman <MiloszKorman@pm.me>
2019-03-29 15:12:38 +0100

    feat(68-fix-swagger): add corsmiddleware

--- backend/scanning_app_api/settings.py
+++ backend/scanning_app_api/settings.py
@@ -39,21 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scanning_app',
     'phonenumber_field',
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
+    'scanning_app',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'scanning_app_api.urls'
