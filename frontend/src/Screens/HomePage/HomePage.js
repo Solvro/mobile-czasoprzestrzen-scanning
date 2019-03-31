6ec2e51b845fc04384358a714073ce08ff0682ff
forestfox128 <basiab217@gmail.com>
2019-03-31 13:56:26 +0200

    bulid: loginlogic done

--- frontend/src/Screens/HomePage/HomePage.js
+++ frontend/src/Screens/HomePage/HomePage.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import SearchContainer from '../../Components/SearchContainer/SearchContainerWithSelect';
 import Table from '../../Components/Table/HomeTable';
 import Layout from '../../Components/Layout/Layout';
-
+import Toolbar from '../../Components/Toolbar/Toolbar';
 import Button from '../../Components/Button/AddButton';
+import InfoDisplay from '../../Components/Displays/InfoDisplay';
 
 class HomePage extends Component {
   render() {
     return (
+      <div className="container">
+            <Toolbar/>
       <Layout layoutDivide={"282"}>
                 <SearchContainer placeholder={"Wyszukaj po nazwie ..."} />
                 <Table />
                 <div className='AddButtonPosition'><Button text={"Dodaj"} link={"/adds"} /></div>
       </Layout>
+      </div>
     );
   }
 }
