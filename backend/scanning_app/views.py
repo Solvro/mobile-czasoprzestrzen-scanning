33faeee54f32f8a92c8d6fcc6d0812ff1e4f917b
Milosz Korman <MiloszKorman@pm.me>
2019-04-02 14:23:01 +0200

    docs(63-sign-up-client-as-unaccepted-add-acceptation): document sign up and creation endpoints

--- backend/scanning_app/views.py
+++ backend/scanning_app/views.py
@@ -1,12 +1,14 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenVerifySerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, \
+    TokenVerifySerializer
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.views import TokenVerifyView
 
-from .models import Equipment, AppUser, RentalInfo
+from .models import Equipment, AppUser, RentalInfo, UnacceptedClient
 from .serializers import EquipmentSerializer, ClientSerializer, \
     RentalInfoSerializer, SignUpClientSerializer, RentalInfoGetSerializer, \
     AdminCreationSerializer, SuperAdminCreationSerializer
@@ -24,8 +26,18 @@ class EquipmentView(viewsets.ModelViewSet):
 
 
 class ClientSignUpView(generics.CreateAPIView):
-    queryset = AppUser.objects.all()
+    queryset = UnacceptedClient.objects.all()
     serializer_class = SignUpClientSerializer
+
+    @swagger_auto_schema(
+        operation_description="POST /api-v1/signup/\n"
+                              "Create new unaccepted client",
+        responses={
+            400: 'Obligatory field not provided or username duplicate'
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 class AdminCreationView(generics.CreateAPIView):
@@ -33,28 +45,35 @@ class AdminCreationView(generics.CreateAPIView):
     serializer_class = AdminCreationSerializer
     permission_classes = (IsSuperAdmin,)
 
-
-class SuperAdminCreationView(generics.CreateAPIView):
-    queryset = AppUser.objects.all()
-    serializer_class = SuperAdminCreationSerializer
-    permission_classes = (IsSuperAdmin,)
-
-
-class ClientSignUpView(generics.CreateAPIView):
-    queryset = AppUser.objects.all()
-    serializer_class = SignUpClientSerializer
-
-
-class AdminCreationView(generics.CreateAPIView):
-    queryset = AppUser.objects.all()
-    serializer_class = AdminCreationSerializer
-    permission_classes = (IsSuperAdmin,)
+    @swagger_auto_schema(
+        operation_description="POST /api-v1/admin/\n"
+                              "Create new admin",
+        responses={
+            400: 'Obligatory field not provided or username duplicate',
+            401: 'No token provided',
+            403: 'User in token doesn\'t have permissions to create admin (Not super admin)'
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 class SuperAdminCreationView(generics.CreateAPIView):
     queryset = AppUser.objects.all()
     serializer_class = SuperAdminCreationSerializer
     permission_classes = (IsSuperAdmin,)
+
+    @swagger_auto_schema(
+        operation_description="POST /api-v1/super-admin/\n"
+                              "Create new super admin",
+        responses={
+            400: 'Obligatory field not provided or username duplicate',
+            401: 'No token provided',
+            403: 'User in token doesn\'t have permissions to create super admin (Not super admin)'
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 
 class ClientView(viewsets.ModelViewSet):
@@ -104,7 +123,9 @@ class VerifyTokenView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         token = request.data['token']
-        validated_token = JWTAuthentication.get_validated_token(JWTAuthentication(), raw_token=token)
-        user = JWTAuthentication.get_user(JWTAuthentication(), validated_token=validated_token)
+        validated_token = JWTAuthentication \
+            .get_validated_token(JWTAuthentication(), raw_token=token)
+        user = JWTAuthentication.get_user(JWTAuthentication(),
+                                          validated_token=validated_token)
         response.data['username'] = user.username
         return response
