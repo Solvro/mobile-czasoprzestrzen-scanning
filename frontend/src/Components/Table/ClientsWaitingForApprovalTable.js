2683ff1638d9b852eef495397b92a6828165a165
Damnjot <236475@student.pwr.edu.pl>
2019-04-03 14:47:32 +0000

    Update frontend/src/Screens/AdminPage/AdminPage.css, frontend/src/Screens/AdminPage/AdminPage.js, frontend/src/Screens/NewAccountPage/NewAccountPage.js files

---
+++ frontend/src/Components/Table/ClientsWaitingForApprovalTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Table from './Table';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ApproveIcon from '@material-ui/icons/Done';
+import Icon from '@material-ui/core/Icon';
+import IconButton from '@material-ui/core/IconButton';
+
+
+function ClientTable (props) {
+
+  const header = ["Nr","Imię i nazwisko","E-mail","Numer telefonu", "Firma", "Zaakceptuj", "Odrzuć"];
+  var rows = []
+
+  for(var i=0; i < 10; i++){
+      rows.push([i,'Jeicam Kujdah', 'a@a.com','888888888', <Icon>clear</Icon>,
+      <IconButton /*className={classes.button}*/ aria-label="Approve">
+        <ApproveIcon />
+      </IconButton>,
+      <IconButton /*className={classes.button}*/ aria-label="Delete">
+        <DeleteIcon />
+      </IconButton>]);
+  }
+   
+  return (
+      <Table header={header} row={rows}/>
+  );
+  
+}
+
+export default ClientTable;
