a8f87337e1947de1545240d28a613397ebde77c8
Starzec <marcel.k@onet.eu>
2019-04-05 14:00:44 +0200

    refactor: deleted unneeded imports

--- backend/scanning_app/models.py
+++ backend/scanning_app/models.py
@@ -1,5 +1,3 @@
-from typing import Any, Tuple, Dict
-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
