3ef6a54ace849c471239200fa041357ec7221cd7
Milosz Korman <MiloszKorman@pm.me>
2019-04-07 12:12:46 +0200

    feat(85-clients-address-and-business-info-as-separate-tables): add address and business_info models

--- backend/scanning_app/models.py
+++ backend/scanning_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.validators import ValidationError
+from django.core.validators import ValidationError, RegexValidator
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
@@ -19,11 +19,43 @@ USER_TYPE = (
 )
 
 
+class Address(models.Model):
+    zip_code_validator = RegexValidator(r'^\d{2}-\d{3}$',
+                                        "Zip code doesn't comply")
+
+    street = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=6, validators=[zip_code_validator])
+    city = models.CharField(max_length=32)
+
+
+class BusinessInfo(models.Model):
+    nip_validator = RegexValidator(
+        r'^((\d{3}[-]\d{3}[-]\d{2}[-]\d{2})|(\d{3}[-]\d{2}[-]\d{2}[-]\d{3}))$',
+        "Nip doesn't comply"
+    )
+    regon_validator = RegexValidator(
+        r'^\d{9}$',
+        "Regon doesn't comply"
+    )
+
+    nip = models.CharField(max_length=13, validators=[nip_validator])
+    regon = models.CharField(max_length=9, validators=[regon_validator])
+
+
 class AppUser(AbstractUser):
     phone = PhoneNumberField()
-    address = models.CharField(null=True, max_length=255)
-    business_data = models.CharField(null=True, max_length=255)
+    address = models.OneToOneField(Address, null=True,
+                                   on_delete=models.SET_NULL)
+    business_data = models.OneToOneField(BusinessInfo, null=True,
+                                         on_delete=models.SET_NULL)
     type = models.CharField(max_length=2, choices=USER_TYPE, default="Cl")
+
+    def delete(self, using=None, keep_parents=False):
+        if self.address is not None:
+            self.address.delete()
+        if self.business_data is not None:
+            self.business_data.delete()
+        return super().delete(using, keep_parents)
 
     def is_client(self):
         return self.type == "Cl"
@@ -57,8 +89,10 @@ class UnacceptedClient(models.Model):
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
     email = models.EmailField(_('email address'), blank=True)
     phone = PhoneNumberField()
-    address = models.CharField(null=True, max_length=255)
-    business_data = models.CharField(null=True, max_length=255)
+    address = models.OneToOneField(Address, null=True,
+                                   on_delete=models.SET_NULL)
+    business_data = models.OneToOneField(BusinessInfo, null=True,
+                                         on_delete=models.SET_NULL)
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
@@ -73,8 +107,21 @@ class UnacceptedClient(models.Model):
         super(UnacceptedClient, self) \
             .save(force_insert, force_update, using, update_fields)
 
+    def delete(self, using=None, keep_parents=False):
+        if self.address is not None:
+            self.address.delete()
+        if self.business_data is not None:
+            self.business_data.delete()
+        return super().delete(using, keep_parents)
+
     def accept(self):
+        address = self.address
+        business_data = self.business_data
         self.delete()
+        if address is not None:
+            address.save()
+        if business_data is not None:
+            business_data.save()
         return AppUser.objects.create(
             username=self.username,
             password=self.password,
@@ -82,8 +129,8 @@ class UnacceptedClient(models.Model):
             last_name=self.last_name,
             email=self.email,
             phone=self.phone,
-            address=self.address,
-            business_data=self.business_data,
+            address=address,
+            business_data=business_data,
             type="Cl",
             is_active=True
         )
--- backend/scanning_app/tests/models.py
+++ backend/scanning_app/tests/models.py
@@ -1,16 +1,16 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from scanning_app.models import AppUser, UnacceptedClient
+from scanning_app.models import AppUser, UnacceptedClient, Address, \
+    BusinessInfo
 
 
-class ModelTestCase(TestCase):
+class AppUserTestCase(TestCase):
     def setUp(self):
         self.client = AppUser(first_name="Marcel",
                               last_name="Krakowiak",
                               email="marcel.k@onet.eu",
-                              phone="793381442",
-                              address="Nowaczka 1/43")
+                              phone="793381442")
 
     def test_model_create_client(self):
         initial_number_of_clients = AppUser.objects.count()
@@ -20,6 +20,22 @@ class ModelTestCase(TestCase):
             initial_number_of_clients + 1,
             after_saving_number_of_clients
         )
+
+    def test_client_deleted_address_and_business_info_deleted(self):
+        address = Address.objects.create(street="Ulica 12/13",
+                                         zip_code="50-330",
+                                         city="Wroclaw")
+        business_info = BusinessInfo.objects.create(nip="725-18-01-126",
+                                                    regon="472836141")
+        client = AppUser.objects.create_user(username=USERNAME,
+                                             password="pass",
+                                             phone="+48732288410",
+                                             address=address,
+                                             business_data=business_info)
+        client.delete()
+        self.assertFalse(UnacceptedClient.objects.exists())
+        self.assertFalse(Address.objects.exists())
+        self.assertFalse(BusinessInfo.objects.exists())
 
 
 USERNAME = "username"
@@ -44,3 +60,56 @@ class UnacceptedClientTests(TestCase):
                           username=USERNAME, password="other pass",
                           phone="+48732450112")
         self.assertEqual(UnacceptedClient.objects.count(), 0)
+
+    def test_client_deleted_address_and_business_info_deleted(self):
+        address = Address.objects.create(street="Ulica 12/13",
+                                         zip_code="50-330",
+                                         city="Wroclaw")
+        business_info = BusinessInfo.objects.create(nip="725-18-01-126",
+                                                    regon="472836141")
+        client = UnacceptedClient.objects.create(username=USERNAME,
+                                                 password="pass",
+                                                 phone="+48732288410",
+                                                 address=address,
+                                                 business_data=business_info)
+        client.delete()
+        self.assertFalse(UnacceptedClient.objects.exists())
+        self.assertFalse(Address.objects.exists())
+        self.assertFalse(BusinessInfo.objects.exists())
+
+    def test_acceptation_doesnt_loose_address_and_business_info(self):
+        pass
+
+
+class AddressTests(TestCase):
+    def test_valid_zip_code_passes(self):
+        valid_zip_code = "50-330"
+        Address.zip_code_validator(valid_zip_code)
+
+    def test_invalid_zip_code_raises_validation_error(self):
+        invalid_zip_code = "5-33"
+        self.assertRaises(ValidationError,
+                          Address.zip_code_validator,
+                          invalid_zip_code)
+
+
+class BusinessInfoTests(TestCase):
+    def test_valid_nip_passes(self):
+        valid_nip = "725-18-01-126"
+        BusinessInfo.nip_validator(valid_nip)
+
+    def test_invalid_nip_doesnt_pass(self):
+        invalid_nip = "725-1-801-126"
+        self.assertRaises(ValidationError,
+                          BusinessInfo.nip_validator,
+                          invalid_nip)
+
+    def test_valid_regon_passes(self):
+        valid_regon = "472836141"
+        BusinessInfo.regon_validator(valid_regon)
+
+    def test_invalid_regon_doesnt_pass(self):
+        invalid_regon = "47283614"
+        self.assertRaises(ValidationError,
+                          BusinessInfo.regon_validator,
+                          invalid_regon)
