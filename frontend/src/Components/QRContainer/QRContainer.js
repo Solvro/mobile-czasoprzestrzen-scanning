bf2b573f125466c204b983e81000f2994efe68d1
forestfox128 <basiab217@gmail.com>
2019-04-13 11:56:11 +0200

    style: edit page styled

--- frontend/src/Components/QRContainer/QRContainer.js
+++ frontend/src/Components/QRContainer/QRContainer.js
@@ -7,11 +7,10 @@ class QRContainer extends React.Component {
 
     render() {
         var qrValue = "rent:" + this.props.rentID;
-        console.log("QR" + qrValue)
         return (
             <Layout layoutDivide={'444'}>
                 <div className="HpQrcode" style={{ padding: '2em' }}>
-                    <QRCode value={qrValue} renderAs={'canvas'} level={'H'} size={120} /></div>
+                    <QRCode value={qrValue} renderAs={'svg'} level={'H'} size={120} /></div>
             </Layout>
         )
     };
