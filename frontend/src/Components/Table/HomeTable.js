af342d6d2d2024bf3972fe2da34cbb68c3e42d5b
Damnjot <3006267-Damnjot@users.noreply.gitlab.com>
2019-04-15 22:20:02 +0000

    Update frontend/src/Components/Table/HomeTable.js

--- frontend/src/Components/Table/HomeTable.js
+++ frontend/src/Components/Table/HomeTable.js
@@ -6,19 +6,7 @@ function HomeTable (props) {
     const header = ["Nr","Nazwa","Typ","Dostępność", "Usuń", "Szczegóły"];
     var rows = props.contains;
 
-<<<<<<< HEAD
-    // for(var i=0; i < 20; i++){
-    //     rows.push([i,"Mikrofon XYZ", "mikrofon",<Icon>done</Icon>,
-    //     <IconButton /*className={classes.button}*/ aria-label="Delete">
-    //         <Icon>edit</Icon>
-    //     </IconButton> ,
-    //     <IconButton /*className={classes.button}*/ aria-label="Delete">
-    //         <DeleteIcon />
-    //     </IconButton>]);
-    // }
-=======
->>>>>>> develop
-     
+
     return (
         <Table header={header} row={rows} height={"700px"}/>
     );
