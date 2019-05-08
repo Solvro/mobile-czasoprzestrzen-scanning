83d9f4670c5f7a410c7b6249208ac09e0f0b38df
JakubGogola <jakubgogola97@gmail.com>
2019-05-08 22:48:47 +0200

    fix: unused imports deleted

--- mobile/src/components/SubmitButton.js
+++ mobile/src/components/SubmitButton.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { View, TouchableOpacity } from 'react-native';
+import { View} from 'react-native';
 import { Text, Button } from 'native-base';
-import { LinearGradient } from 'expo';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import buttonStyles from '../styles/ButtonStyles.js';
