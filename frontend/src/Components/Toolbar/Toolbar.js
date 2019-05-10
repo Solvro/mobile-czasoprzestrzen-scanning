f40cbea309b826229fa24f6afe39d232656bd9b8
forestfox128 <basiab217@gmail.com>
2019-05-10 11:08:38 +0200

    feature: new admin menu

--- frontend/src/Components/Toolbar/Toolbar.js
+++ frontend/src/Components/Toolbar/Toolbar.js
@@ -1,34 +1,135 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Toolbar.css';
-import classes from './Toolbar.css';
-import Icon from '@material-ui/core/Icon';
 import logo from '../../assests/czasoprzestrzen_logoW.png';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import DButton from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import { withStyles } from '@material-ui/core/styles';
+const styles = theme => ({
+    buttonToolbar: {
+        cursor: 'pointer',
+        backgroundColor: '#224f77',
+        color: 'white',
+        border: 'none',
+        textDecoration: 'none',
+        padding: '1.8em',
+        "&:hover": {
+            //you want this to be the same as the backgroundColor above
+            backgroundColor: "#163550",
+            border: '#163550'
+        }
+    },
+  });
+    class Toolbar extends React.Component{
 
+        state = {
+            open: false,
+          };
+        
+          handleToggle = () => {
+            this.setState(state => ({ open: !state.open }));
+          };
+        
+          handleClose = event => {
+            if (this.anchorEl.contains(event.target)) {
+              return;
+            }
+            this.setState({ open: false });
+          };
 
-
-const toolbar = ( props ) => (
+          handleOtherTableClick = () => {
+            this.props.history.push('/home');
+            this.handleClose();
+          }
+        
+          render() {
+            const { classes } = this.props;
+            const { open } = this.state;
+        
+            return (
     <header className='Toolbar'>
         <div>
             <img src={logo} className='Logo' alt="Logo" />
         </div>
         <nav className='DesktopOnly'>
             <ul className='NavigationItems'>
-            <li className='NavigationItem'>
-                <Link className={props.active ? classes.active : null} to="/home">Sprzęt</Link>
+            <li className='linkInMenu'>
+                <DButton
+                    className={classes.buttonToolbar}
+                    aria-owns={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true">
+                   <Link to="/home" style={{textDecoration:'none',border: 'none'}}>Sprzęt</Link> 
+                </DButton>
+                
             </li>
-            <li className='NavigationItem'>
-                <Link className={props.active ? classes.active : null} to="/rents">Wypożyczenia</Link>
+            <li className='linkInMenu'>
+                <DButton
+                    className={classes.buttonToolbar}
+                    aria-owns={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true">
+                   <Link to="/rents" style={{textDecoration:'none',border: 'none'}}>Wypożyczenia</Link> 
+                </DButton>
+                
             </li>
-            <li className='NavigationItem'>
-                <Link className={props.active ? classes.active : null} to="/clients">Klienci</Link>
+            <li className='linkInMenu'>
+                <DButton
+                    className={classes.buttonToolbar}
+                    aria-owns={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true">
+                   <Link to="/clients" style={{textDecoration:'none',border: 'none'}}>Klienci</Link> 
+                </DButton>
+                
             </li>
-            <li className='NavigationItem' style={{paddingBottom: '1.8em'}}>
-                <Link className={props.active ? classes.active : null} to="/account"><Icon>account_box</Icon></Link>
+            <li className='linkInMenu' >
+                                
+                <DButton
+                    buttonRef={node => {
+                    this.anchorEl = node;
+                    }}
+                    className={classes.buttonToolbar}
+                    aria-owns={open ? 'menu-list-grow' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleToggle}>
+                    Zaawansowane
+                </DButton>
+          <Popper open={open} anchorEl={this.anchorEl} transition disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                id="menu-list-grow"
+                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+              >
+                <Paper>
+                <ClickAwayListener onClickAway={this.handleClose}>
+                    <MenuList>
+                    <Link className="linkInMenu" to='/account'>
+                        <MenuItem>Inne</MenuItem>
+                      </Link>
+                      <Link className="linkInMenu" to='/createNewAccount'>
+                        <MenuItem>Stwórz nowego administratora</MenuItem>
+                      </Link>
+                      <Link  className="linkInMenu" to='/changePassword'>
+                        <MenuItem>Zmień hasło</MenuItem>
+                      </Link>
+                      <Link className="linkInMenu" to='/login'>
+                        <MenuItem onClick={()=>localStorage.clear()} >Wyloguj</MenuItem>
+                      </Link>
+                    </MenuList>
+                    </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
             </li>
             </ul>
         </nav>
     </header>
 );
+    }}
 
-export default toolbar;
+    export default withStyles(styles)(Toolbar);
