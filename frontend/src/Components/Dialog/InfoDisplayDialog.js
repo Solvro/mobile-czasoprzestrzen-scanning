cbc8acc39adc8554536ce506367059479a4b4186
Damnjot <236475@student.pwr.edu.pl>
2019-05-13 17:54:30 +0200

    idk

--- frontend/src/Components/Dialog/InfoDisplayDialog.js
+++ frontend/src/Components/Dialog/InfoDisplayDialog.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button } from '@material-ui/core';
 
 function AlertDialog (props) {
 
     return (
       <div>
-        <Dialog
-          open={props.dialogOpen}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-            <DialogTitle id="alert-dialog-title">
-                {props.message}
-            </DialogTitle>
-            <p>
-                {props.bussinessData.id}}
-            </p>
-        </Dialog>
+        
+          <Dialog
+            onEscapeKeyDown={alert("la")}
+            open={props.dialogOpen}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+              <DialogTitle id="alert-dialog-title">
+                  {props.message}
+              </DialogTitle>
+              <p>
+                  {props.bussinessData.id}}
+              </p>
+              <Button onClick={alert("aaa")}> exit</Button>
+          </Dialog>
       </div>
     );
   
