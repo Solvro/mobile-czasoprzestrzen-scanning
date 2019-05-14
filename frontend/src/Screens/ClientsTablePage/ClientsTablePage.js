d977703cb6c8220750135efe7a63299ddd90e36d
Damnjot <236475@student.pwr.edu.pl>
2019-05-14 15:24:14 +0200

    fix: applied fix for clients table

--- frontend/src/Screens/ClientsTablePage/ClientsTablePage.js
+++ frontend/src/Screens/ClientsTablePage/ClientsTablePage.js
@@ -23,20 +23,24 @@ class Clients extends Component {
       infoMessage: '',
       errorMsg: '',
       dialogOpen: false,
-      clickedItemId: ''
+      clientsList: ''
     }
   }
 
   async componentDidMount(){
     await getClientsList().then((res)=>{
-      this.setState({isLoading: false});
-      this.createTable(res);
+      this.setState({
+        isLoading: false,
+        clientsList: res
+      });
+      alert(this.state.clientsList)
+      this.createTable(this.state.clientsList);
     })    
   }
 
-  async filterTableContentByNameContains(nameFragment){
-    await getClientsList().then((res)=>{
-
+  filterTableContentByNameContains(nameFragment){
+    alert(this.state.clientsList)
+      var res = this.state.clientsList
       var filteredRes = Object.keys(res).filter(function(params) {
         return res[params]
           .username
@@ -46,7 +50,7 @@ class Clients extends Component {
         return res[i];
       })
       this.createTable(filteredRes)
-    })    
+    
   }
 
   createTable(res){
