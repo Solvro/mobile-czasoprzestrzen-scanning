c6ac021fbacb7f75705e1563ece49f8ea3fc3669
Damnjot <236475@student.pwr.edu.pl>
2019-05-26 20:26:12 +0200

    fix applied

--- frontend/src/Components/Dialog/BussinessInfoDisplayDialog.js
+++ frontend/src/Components/Dialog/BussinessInfoDisplayDialog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import {Button} from '@material-ui/core';
+import {Button, DialogContent, DialogActions} from '@material-ui/core';
 
 function AlertDialog (props) {
 
@@ -12,17 +12,20 @@ function AlertDialog (props) {
             open={props.dialogOpen}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
-          >
-              <DialogTitle id="alert-dialog-title"  style={{backgroundColor: "#ebf3f9"}}>
+            fullWidth="fullWidth"
+            maxWidth="sm"
+          > 
+              <DialogTitle id="alert-dialog-title"  style={{backgroundColor: "#e6edff"}}>
                   {props.message}
               </DialogTitle>
-              <div style={{padding: "20px"}}>
-                <p> Nazwa: {props.bussinessData.name==null ? "brak" : props.bussinessData.name} </p>
-                <p> NIP: {props.bussinessData.nip==null ? "brak" : props.bussinessData.nip} </p>
-                <p> Regon: {props.bussinessData.regon==null ? "brak" : props.bussinessData.regon} </p>
-                
+              <DialogContent>
+                  <p> Nazwa: {props.bussinessData.name==null ? "brak" : props.bussinessData.name} </p>
+                  <p> NIP: {props.bussinessData.nip==null ? "brak" : props.bussinessData.nip} </p>
+                  <p> Regon: {props.bussinessData.regon==null ? "brak" : props.bussinessData.regon} </p>
+              </DialogContent>
+              <DialogActions>
                 <Button onClick={props.handleClose} color="primary"> zamknij </Button>
-              </div>
+              </DialogActions>
           </Dialog>
       </div>
     );
